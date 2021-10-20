601732b962c5ae01519e7e6e075d7992dcebceb6
George R. NICA <77013090+kiinoda@users.noreply.github.com>
2021-10-20 23:31:29 +0300

    clean up code that was unused

--- src/kemal.cr
+++ src/kemal.cr
@@ -25,41 +25,6 @@ operations.each do |op|
   end
 end
 
-# get "/deploy/:setup/branch/:branch" do |env|
-#   setup = env.params.url["setup"]
-#   branch = env.params.url["branch"]
-#   # "Deploy #{setup} on branch #{branch}"
-#   output = ""
-#   ["sleep 20", "echo Hello! 100"].each do |p|
-#     cmd = p.split[0]
-#     args = p.split[1..]
-#     stdout = IO::Memory.new
-#     process = Process.new(cmd, args, output: stdout)
-#     status = process.wait
-#     output += stdout.to_s
-#   end
-#   output
-# end
-
-# get "/sleep/:time/say/:word" do |env|
-#   time = env.params.url["time"]
-#   word = env.params.url["word"]
-#   output = ""
-#   ["sleep #{time}", "echo #{word}"].each do |p|
-#     cmd = p.split[0]
-#     args = p.split[1..]
-#     stdout = IO::Memory.new
-#     process = Process.new(cmd, args, output: stdout)
-#     status = process.wait
-#     if cmd == "sleep"
-#       output += "Slept #{time} seconds. "
-#     else
-#       output += stdout.to_s
-#     end
-#   end
-#   output
-# end
-
 [404, 500].each do |e|
   error e do
     "Go away!"
