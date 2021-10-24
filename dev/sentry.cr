ce45a38612aca2da0c18ab6e3a1d95f2467fa818
George R. NICA <grn@infinium.io>
2021-10-24 22:28:21 +0300

    move sentry outside, as separate dev tooling

--- dev/sentry.cr
+++ dev/sentry.cr
@@ -2,8 +2,8 @@ require "sentry"
 
 sentry = Sentry::ProcessRunner.new(
   display_name: "Kemal Dev",
-  build_command: "crystal build ./src/kemal.cr -o ./bin/kemal",
-  run_command: "./bin/kemal",
+  build_command: "crystal build ./src/renzu.cr -o ./bin/renzu",
+  run_command: "./bin/renzu",
   files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.yml"]
 )
 sentry.run
