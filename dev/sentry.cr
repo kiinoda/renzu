1e6c818d3b532bacda32ed3ec1774c0fddd0c0b6
George R. NICA <grn@infinium.io>
2022-07-13 20:23:36 +0300

    update sentry to monitory .yml config in project root

--- dev/sentry.cr
+++ dev/sentry.cr
@@ -4,6 +4,6 @@ sentry = Sentry::ProcessRunner.new(
   display_name: "Kemal Dev",
   build_command: "crystal build ./src/renzu.cr -o ./bin/renzu",
   run_command: "./bin/renzu",
-  files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.yml"]
+  files: ["./src/**/*.cr", "./src/**/*.ecr", "./src/**/*.yml", "*.yml"]
 )
 sentry.run
